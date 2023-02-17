355216924ababd75a8ecd4586f7561a4e5b29c97
JCzyszczon <jakub.czyszczon1@gmail.com>
2023-02-17 19:39:40 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import {Routes, Route} from 'react-router-dom';
-import Home from './pages/index.js';
-import Navbar from './pages/navbar.js';
+import Home from './pages/index.jsx';
+import Navbar from './pages/navbar.jsx';
 
 function App() {
   return (
     <>
       <Navbar/>
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/mauve-beauty' element={<Home/>}/>
       </Routes>
     </>
   );
